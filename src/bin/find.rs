74c2a23c3c6f49d7e7b65d975bb4143387febd69
Sebastian K <s3bk@protonmail.com>
2021-01-20 19:23:39 +0300

    add support for multiple regexes and add docs

--- src/bin/find.rs
+++ src/bin/find.rs
@@ -7,14 +7,17 @@ use regex_ring::{RingSearcher, Match};
 fn main() {
     let mut args = env::args();
     let _ = args.next().expect("no program name");
-    let regex_s = args.next().expect("no regex");
 
-    let regex = RegexBuilder::new().build(&regex_s).expect("invalid regex");
-    let mut searcher = RingSearcher::new(regex, 1024 * 1024);
+    let mut searcher = RingSearcher::new(1024);
+
+    for regex_str in args {
+        searcher.add_regex_str(&regex_str).expect("invalid regex");
+    }
 
     for b in io::stdin().lock().bytes() {
-        if let Some(m) = searcher.push(b.unwrap()) {
-            println!("{:?}", m);
+        searcher.push(b.unwrap());
+        for (i, m) in searcher.matches() {
+            println!("#{} {:?}", i, m);
             let (head, tail) = searcher.match_data(&m);
             if let (Ok(a), Ok(b)) = (str::from_utf8(head), str::from_utf8(tail)) {
                 println!("> {}{}", a, b);
--- src/lib.rs
+++ src/lib.rs
@@ -1,65 +1,125 @@
-use regex_automata::{Regex, DFA};
+use regex_automata::{Regex, RegexBuilder, DFA, DenseDFA};
 use std::collections::VecDeque;
 
-pub struct RingSearcher<D: DFA> {
+// state to keep for each Regex
+struct Search<D: DFA> {
     regex: Regex<D>,
-    buffer: VecDeque<u8>,
     state_id: D::ID,
     was_match: bool,
+    is_match: bool,
+}
+
+#[derive(Debug)]
+pub enum Error {
+    InvalidRegex
+}
+
+/// A Ringbuffer backed steam searcher
+/// 
+/// Usage:
+///  1. create a searcher using `new`.
+/// 
+///  2. Add all regexes to search for with `add_regex` or `add_regex_str`.
+/// 
+///  3. For every input byte:
+///     Call `push` with the input, then call `matches` to obtain matches or `matches_string` to obtain match strings.
+/// 
+///  4. To get the input data for a match: call `match_data` or `match_string`.
+///     This should happen before the next call to `push` to avoid overwriting the data for this match.
+/// 
+pub struct RingSearcher<D: DFA> {
+    buffer: VecDeque<u8>,
     position: usize,
+    searches: Vec<Search<D>>,
+    buffer_size: usize,
 }
 
 impl<D: DFA> RingSearcher<D> {
-    pub fn new(regex: Regex<D>, buffer_size: usize) -> Self {
+    /// Create a ringbuffer backed regex stream searcher with the given ringbuffer size.
+    /// The size should exeed the longest expected match.
+    pub fn new(buffer_size: usize) -> Self {
         RingSearcher {
-            state_id: regex.forward().start_state(),
-            regex,
+            searches: vec![],
             buffer: VecDeque::with_capacity(buffer_size),
-            was_match: false,
             position: 0,
+            buffer_size,
         }
     }
 
-    pub fn push(&mut self, input: u8) -> Option<Match> {
-        if self.buffer.len() + 1 > self.buffer.capacity() {
+    /// add a Regex to search for
+    pub fn add_regex(&mut self, regex: Regex<D>) {
+        let state_id = regex.forward().start_state();
+        self.searches.push(Search {
+            state_id,
+            is_match: regex.forward().is_match_state(state_id),
+            regex,
+            was_match: false,
+        });
+    }
+
+    /// feed one stream byte to the searcher
+    /// `matches` or `matches_string` must be called to obtain the matches ending at the *previous* input byte.
+    pub fn push(&mut self, input: u8) {
+        if self.buffer.len() + 1 > self.buffer_size {
             self.buffer.pop_front();
         }
         self.buffer.push_back(input);
-
-        let dfa = self.regex.forward();
-        let mut state_id = dfa.next_state(self.state_id, input);
-        let is_match = dfa.is_match_state(state_id);
-        let was_match = self.was_match;
-        let position = self.position;
-
-        if dfa.is_dead_state(state_id) {
-            state_id = dfa.start_state();
-        }
-
-        // update state
-        self.was_match = is_match;
-        self.state_id = state_id;
         self.position += 1;
 
-        match (was_match, is_match) {
-            (true, false) => {
-                self.regex.reverse().rfind_iter(self.buffer.iter().rev().cloned().skip(1)).map(move |len| {
-                    let start = if len == self.buffer.len() {
-                        None
-                    } else {
-                        Some(position - len)
-                    };
+        for search in &mut self.searches {
+            let dfa = search.regex.forward();
+            let mut state_id = dfa.next_state(search.state_id, input);
+            let is_match = dfa.is_match_state(state_id);
 
-                    Match {
-                        start,
-                        end: position,
-                    }
-                })
+            if dfa.is_dead_state(state_id) {
+                state_id = dfa.start_state();
             }
-            _ => None
+
+            // update state
+            search.was_match = search.is_match;
+            search.is_match = is_match;
+            search.state_id = state_id;
         }
     }
 
+    pub fn finish(&mut self) {
+
+    }
+
+    /// obtain the matches ending at the previous input byte
+    pub fn matches(&self) -> impl Iterator<Item=(usize, Match)> + '_ {
+        let position = self.position;
+        self.searches.iter().enumerate().filter_map(move |(i, search)| {
+            match (search.was_match, search.is_match) {
+                (true, false) => {
+                    search.regex.reverse().rfind_iter(self.buffer.iter().rev().cloned().skip(1)).map(move |len| {
+                        let start = if len == self.buffer.len() {
+                            None
+                        } else {
+                            Some(position - len - 1)
+                        };
+
+                        (i, Match {
+                            start,
+                            end: position - 1,
+                        })
+                    })
+                }
+                _ => None
+            }
+        })
+    }
+
+    /// Same as `matches` but returns the match string
+    /// 
+    /// Warning: This function allocates for every match.
+    /// Use for debugging only.
+    pub fn matches_string(&self) -> impl Iterator<Item=(usize, String)> + '_ {
+        self.matches().map(move |(i, m)| (i, self.match_string(&m)))
+    }
+
+    /// Obtain the data for a specific match, as far as it is still in the buffer.
+    /// Data is obtained as a pair of slices to avoid copying.
     pub fn match_data(&self, match_: &Match) -> (&[u8], &[u8]) {
         let (head, tail) = self.buffer.as_slices();
 
@@ -72,7 +132,23 @@ impl<D: DFA> RingSearcher<D> {
         // position of match end in the buffer
         let end = match_.end - offset;
         
-        (slice_window(head, start, end), slice_window(tail, start, end))
+        (slice_window(head, start, end), slice_window(tail, start.saturating_sub(head.len()), end.saturating_sub(head.len())))
+    }
+
+    /// Obtain the String for a specific match, as far as it is still in the buffer.
+    /// Warning: Allocates.
+    pub fn match_string(&self, match_: &Match) -> String {
+        let (head, tail) = self.match_data(match_);
+        format!("{}{}", String::from_utf8_lossy(head), String::from_utf8_lossy(tail))
+    }
+}
+
+impl RingSearcher<DenseDFA<Vec<usize>, usize>> {
+    /// convinience function to add Regex from a `str`.
+    pub fn add_regex_str(&mut self, regex_str: &str) -> Result<(), Error> {
+        let regex = RegexBuilder::new().build(regex_str).map_err(|e| Error::InvalidRegex)?;
+        self.add_regex(regex);
+        Ok(())
     }
 }
 
@@ -80,6 +156,10 @@ fn slice_window(slice: &[u8], start: usize, end: usize) -> &[u8] {
     &slice[start.min(slice.len()) .. end.min(slice.len())]
 }
 
+/// Match object.
+/// 
+/// Contains the stream positions of the match.
+/// If the start of a match could not be found, `start` will be `None`.
 #[derive(Copy, Clone, Debug)]
 pub struct Match {
     pub start: Option<usize>,
